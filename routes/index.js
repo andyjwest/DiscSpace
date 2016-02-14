3d6f68dab88d7a8e51c2384b7dae14642d220b9a
Andy West <andyjwest@gmail.com>
2016-02-14 15:35:01 -0500

    Adding some endpoints for player and tournament

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,31 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var name= req.param("name");
+
+  //TODO get the values from the DB based on what we wnat
+  res.json({message: "test"});
 });
+
+router.get('/tournament', function(req, res, next) {
+  //TODO get the values from the DB based on what we wnat
+  res.json({tournament:
+      [{name: "test"},
+        {name:"anotherOne"}]
+  });
+});
+
+router.get('/tournament/{tournament-id}', function(req, res, next) {
+  //TODO get the values from the DB based on what we wnat
+  res.json({message: "test"});
+});
+
+router.get('/player', function(req, res, next) {
+  var name= req.param("name");
+
+  //TODO get the values from the DB based on what we wnat
+  res.json({message: "test"});
+});
+
 
 module.exports = router;
